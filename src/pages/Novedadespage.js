86518baf24dc6c0236422056add7fe1b661a6adb
Cecilia Mendoza <macemendoza79@gmail.com>
2024-03-29 19:49:35 -0300

    Paginas Novedades y Contacto

--- src/pages/Novedadespage.js
+++ src/pages/Novedadespage.js
@@ -1,6 +1,14 @@
 import React from "react";
 const Novedadespage= (props) => {
   return (
-       <div>Novedadespage</div>);
+    <main className="holder">
+     <div className="Novedades">
+        <h2>Novedades</h2>
+        <h3>Titulo</h3>
+        <h4>Subtitulo</h4>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.  </p>
+      </div>
+    </main>
+  );
 }
 export default Novedadespage;
